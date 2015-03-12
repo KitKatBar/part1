f60992074579ca39a26c2bf2e4eaf6c3987272fe
Jiu <jiu@hawaii.edu>
2015-03-12 12:51:05 +0900

    testing timezone

---
+++ src/part1/GenerateICS.java
@@ -0,0 +1,78 @@
+package part1;
+
+
+import java.io.*;
+import java.util.*;
+
+public class GenerateICS {
+
+	public static void main(String[] args) {
+		
+		try{
+			
+			//create blank ics file
+			File file = new File("studyfinal.ics");
+			
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			System.out.print("");
+			bw.write("BEGIN:VCALENDAR");
+			bw.newLine();
+
+			//VERSION
+			bw.write("VERSION:2.0");
+			bw.newLine();
+			
+			//create new event
+			bw.write("BEGIN:VEVENT");
+			bw.newLine();		
+			
+			//CLASSIFICATION
+			
+			bw.write("CLASSIFICATION:");			
+			bw.newLine();
+			
+			//LOCATION
+	
+			bw.write("LOCATION:");			
+			bw.newLine();
+			
+			//PRIORITY
+			
+			
+			//SUMMARY
+			
+			
+			//DTSTART
+			
+			
+			//DTEND
+			
+			
+			bw.write("END:VEVENT");
+			bw.newLine();		
+			
+			//Time zone identifier
+			bw.write("BEGIN:VTIMEZONE");
+			bw.newLine();					
+			
+			bw.write("TZID:");
+			bw.write(TimeZone.getDefault().getDisplayName());
+			bw.newLine();
+			
+			bw.write("END:VTIMEZONE");
+			bw.newLine();	
+			
+			
+			
+			bw.write("END:VCALENDAR");
+			bw.close();
+			  
+		}catch(IOException e){
+			System.out.println(e);
+		}
+		
+		
+	}
+}
