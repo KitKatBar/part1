117a2eac29f2551c271acd07734b0c15687b16f3
Jiu <jiu@hawaii.edu>
2015-03-13 09:41:41 +0900

    added priority

--- src/part1/GenerateICS.java
+++ src/part1/GenerateICS.java
@@ -18,6 +18,7 @@ public class GenerateICS {
 			FileWriter fw = new FileWriter(file);
 			BufferedWriter bw = new BufferedWriter(fw);
 			
+			//begin calendar
 			System.out.print("");
 			bw.write("BEGIN:VCALENDAR");
 			bw.newLine();
@@ -43,8 +44,8 @@ public class GenerateICS {
 			bw.newLine();
 	
 			//PRIORITY
-						
-
+			bw.write("PRIORITY:"+getPriority());
+			bw.newLine();
 			
 			//DTSTART
 			
@@ -59,8 +60,7 @@ public class GenerateICS {
 			bw.write("BEGIN:VTIMEZONE");
 			bw.newLine();					
 			
-			bw.write("TZID:");
-			bw.write(TimeZone.getDefault().getID());
+			bw.write("TZID:"+TimeZone.getDefault().getID());
 			bw.newLine();
 			
 			bw.write("END:VTIMEZONE");
@@ -86,10 +86,46 @@ public class GenerateICS {
 	}
 	
 	public static String getLocation() {
-		   String location;
+		String location;
 
-		   System.out.print("Please enter the location for this event: ");
-		   location = in.nextLine();
-		   return location;
+		System.out.print("Please enter the location for this event: ");
+		location = in.nextLine();
+		return location;
+	}
+	
+	public static int getPriority() {
+		int priority = 0;
+		String yesno;
+		
+		while(true){
+			System.out.print("Is there priority for this event?(y/n): ");
+			yesno = in.nextLine();
+			
+			if(yesno.equals("n")){ //no priority
+				return priority;
+			}
+			else if(yesno.equals("y")){ //there is priority
+				
+				while(true){
+					System.out.print("Please enter priority for this event (1 for highest, 9 for lowest): ");
+					
+					priority = in.nextInt();
+					
+					
+					if(priority > 0 && priority < 10){ //if it is between 1-9, return
+						return priority;
+					}
+					else{
+						System.out.println("Please enter a number from 1 to 9.");
+					}
+				}
+				
+			}
+			else{ //if user inputs something other than y/n
+				System.out.println("Please enter \"y\" or \"n\".");
+			}
 		}
+
+	}
+	
 }
